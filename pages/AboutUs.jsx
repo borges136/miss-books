ffc597042e90ca61e15e32eff34d8d3968a1ed25
Boris Martzinovsky <borges136@gmail.com>
2025-02-10 23:16:09 +0200

    fix hero image, remove unneeded about sub-components

--- pages/AboutUs.jsx
+++ pages/AboutUs.jsx
@@ -7,15 +7,6 @@ export function AboutUs() {
 
             <h1>About books and us...</h1>
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio dolore sapiente, iste animi corporis nisi atque tempora assumenda dolores. Nobis nam dolorem rerum illo facilis nemo sit voluptatibus laboriosam necessitatibus!</p>
-            <section>
-                <nav>
-                    <Link to="/about/team" >Team</Link>
-                    <Link to="/about/vision" >Vision</Link>
-                </nav>
-            </section>
-            <section>
-                <Outlet />
-            </section>
         </section>
     )
 }
--- pages/HomePage.jsx
+++ pages/HomePage.jsx
@@ -4,7 +4,7 @@ export function HomePage() {
     return (
         <section className="home-page">
             <h1 >Book's R Us!</h1>
-            <img src="../assets/img/react.png" alt="hero-image" />
+            <img src="assets/img/react.png" alt="hero-image" />
         </section>
     )
 }
